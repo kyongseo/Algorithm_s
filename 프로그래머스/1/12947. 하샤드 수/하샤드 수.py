3574faa9185bd30c22f4e50eb6f3e94543bc32ad
kyongseo <102573192+kyongseo@users.noreply.github.com>
2024-03-17 21:09:16 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -1,2 +1,11 @@
 def solution(x):
-    return x % sum([int(c) for c in str(x)]) == 0
+    num = 0
+    for i in list(str(x)):
+        num += int(i) #1
+    if x % num == 0: # 10 % 1 == 0 나머지
+        answer = True 
+    else:
+        answer = False
+    return answer
+
+# return x % sum([int(c) for c in str(x)]) == 0
