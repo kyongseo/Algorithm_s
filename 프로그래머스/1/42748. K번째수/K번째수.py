2ecc3a0e72daa41c08f9e02af3d74285ce4df982
kyongseo <102573192+kyongseo@users.noreply.github.com>
2024-03-23 09:47:34 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,6 +1,9 @@
-
 def solution(array, commands):
+
     answer = []
     for com in commands:
-        answer.append(sorted(array[com[0]-1:com[1]])[com[2]-1])
+        arr = array[com[0]-1: com[1]]
+        arr.sort()
+        answer.append(arr[com[2]-1])
+        
     return answer
