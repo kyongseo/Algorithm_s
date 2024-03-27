bad96b9d9a8ebcb8c27b8e2afe9e7c8ab4cb429a
kyongseo <102573192+kyongseo@users.noreply.github.com>
2024-03-27 15:34:33 +0900

    [level 2] Title: 구명보트, Time: 8.28 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -2,14 +2,14 @@ def solution(people, limit):
     
     people.sort()
     start, end = 0, len(people)-1
-    count = 0
+    answer = 0
     
     while start <= end :
-        count+=1
+        answer+=1
         
         if people[start] + people[end] <= limit:
             start += 1
             end -= 1
         else:
             end -= 1
-    return count
+    return answer
