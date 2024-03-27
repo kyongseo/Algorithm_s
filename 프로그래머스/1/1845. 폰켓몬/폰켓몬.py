5c5a7d64e3060a7dad78a17582fb7ef84d7f968b
kyongseo <102573192+kyongseo@users.noreply.github.com>
2024-03-27 13:06:53 +0900

    [level 1] Title: 폰켓몬, Time: 0.28 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -0,0 +1,13 @@
+def solution(nums):
+    answer = 0
+    chosen_length=len(nums)//2
+    tmp=set(nums)
+    num_li=list(tmp)
+    answer_leng=len(num_li)
+    
+    if chosen_length<answer_leng:
+        answer=chosen_length
+    else:
+        answer=answer_leng
+    return answer
+            
