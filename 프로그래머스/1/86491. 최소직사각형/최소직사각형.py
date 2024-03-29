74f21a0c8197032551f1bacad0b8a0665a157b59
kyongseo <102573192+kyongseo@users.noreply.github.com>
2024-03-29 11:26:29 +0900

    [level 1] Title: 최소직사각형, Time: 3.03 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,15 +1,12 @@
 def solution(sizes):
-    w = [] # 가로 값을 담을 빈 리스트 생성
-    h = [] # 세로 값
-    for i in range(len(sizes)): # sizes는 2차원 리스트
-        
-        if sizes[i][0] >= sizes[i][1]: # 조건 : 가로>=세로
+    w = []
+    h = []
+    for i in range(len(sizes)):
+        if sizes[i][0] >= sizes[i][1]:
+            w.append(sizes[i][0])
+            h.append(sizes[i][1])
+        else:
+            h.append(sizes[i][0])
+            w.append(sizes[i][1])
             
-            w.append(sizes[i][0]) 
-            h.append(sizes[i][1]) 
-            
-        else: # 명함번호2 해당
-            h.append(sizes[i][0]) 
-            w.append(sizes[i][1]) 
-
     return max(w) * max(h)
