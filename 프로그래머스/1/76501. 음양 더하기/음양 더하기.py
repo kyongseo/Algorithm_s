ceefbeb729b6f845d9d3a0598eee5c4331e56d81
kyongseo <102573192+kyongseo@users.noreply.github.com>
2024-03-31 22:52:34 +0900

    [level 1] Title: 음양 더하기, Time: 0.14 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,8 +1,8 @@
 def solution(absolutes, signs):
     answer = 0
-    for i in range(len(absolutes)):
-        if signs[i]:
-            answer += absolutes[i]
-        else:
-            answer -= absolutes[i]
+    for i in range(len(absolutes)): #0,1,2
+        if signs[i]: # 4, 12
+            answer += absolutes[i] # 4+ 12
+        else: # -7
+            answer -= absolutes[i] # -7
     return answer
