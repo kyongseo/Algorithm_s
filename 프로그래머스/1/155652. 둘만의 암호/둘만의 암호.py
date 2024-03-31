7a06cfd6be33369cd4e8aac3d64e6b7b35a2890c
kyongseo <102573192+kyongseo@users.noreply.github.com>
2024-03-31 23:14:55 +0900

    [level 1] Title: 둘만의 암호, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -1,9 +1,9 @@
 def solution(s, skip, index):
-    alp = "abcdefghijklmnopqrstuvwxyz"
+    alp = "abcdefghijklmnopqrstuvwxyz" #26
     answer = ""
     for i in list(skip):
-        alp = alp.replace(i,"")
+        alp = alp.replace(i,"") #acefg... bd
         
     for a in s:
-        answer += alp[(alp.find(a) + index) % len(alp)]
+        answer += alp[(alp.find(a) + index) % len(alp)] #21
     return answer
