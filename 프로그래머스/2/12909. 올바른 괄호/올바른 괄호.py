4e7c5317b2606a58c7b1b6e6d38255180cfba9b7
kyongseo <102573192+kyongseo@users.noreply.github.com>
2024-04-08 17:44:31 +0900

    [level 2] Title: 올바른 괄호, Time: 7.08 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -4,11 +4,11 @@ def solution(s):
         if i == '(':
             stack.append(i)
         else:
-            if stack == []: # 오른쪽 괄호로 시작할경우
+            if stack == []:
                 return False
             else:
                 stack.pop()
-    if stack != []: # 다 끝났는데 왼쪽 괄호가 스택에 남아있을경우
+    if stack != []: 
         return False
     return True
 
