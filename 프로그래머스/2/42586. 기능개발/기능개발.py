e467cf712c7997f9a53187b3a403f99aa8cac316
kyongseo <102573192+kyongseo@users.noreply.github.com>
2024-04-09 16:57:36 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,17 +1,13 @@
-from collections import deque
 def solution(progresses, speeds):
     answer = []
     while progresses:
         cnt = 0
         for i in range(len(progresses)):
             progresses[i] += speeds[i]
-        if progresses[0] >= 100:
-            while progresses:
-                if progresses[0] >= 100:
-                    progresses.pop(0)
-                    speeds.pop(0)
-                    cnt += 1
-                else:
-                    break
+        while progresses and progresses[0] >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            cnt += 1
+        if cnt > 0:
             answer.append(cnt)
     return answer
