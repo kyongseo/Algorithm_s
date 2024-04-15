dbfe390dd956d29977c4c0d8868d0d7c046044d2
kyongseo <102573192+kyongseo@users.noreply.github.com>
2024-04-15 16:49:10 +0900

    [level 2] Title: 프로세스, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,13 @@
+from collections import deque
+def solution(prioriries, location):
+    answer = 0
+    d = deque([(v, i) for i,v in enumerate(prioriries)])
+    while d:
+        item = d.popleft()
+        if d and max(d)[0] > item[0]:
+            d.append(item)
+        else:
+            answer += 1
+            if item[1] == location:
+                break
+    return answer
