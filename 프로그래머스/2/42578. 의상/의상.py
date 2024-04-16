7241253b8bb89d8ecaaaefa0ee562e5b756df7dc
kyongseo <102573192+kyongseo@users.noreply.github.com>
2024-04-16 17:42:07 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -0,0 +1,13 @@
+def solution(clothes):
+    answer = 1
+    dic = {}
+    for i in clothes:
+        if i[1] in dic:
+            dic[i[1]] += 1
+        else:
+            dic[i[1]] = 1
+    for i in dic.keys():
+        answer *= dic[i] +1
+    answer -= 1
+
+    return answer
