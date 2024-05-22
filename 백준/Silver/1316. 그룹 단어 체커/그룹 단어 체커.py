d47bff6d9a26a9caa819dc5d0d900cbab7719d76
kyongseo <102573192+kyongseo@users.noreply.github.com>
2024-05-22 09:34:47 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,12 @@
+N = int(input())
+cnt = N
+
+for i in range(N):
+    word = input() 
+    for j in range(len(word)-1):
+        if word[j] == word[j+1]:
+            continue
+        elif word[j] in word[j+1:]:
+            cnt -= 1
+            break
+print(cnt)
