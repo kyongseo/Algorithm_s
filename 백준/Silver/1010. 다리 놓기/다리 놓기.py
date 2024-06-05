3edc41342c848c21a30d1249cbcf055eda975a6e
kyongseo <102573192+kyongseo@users.noreply.github.com>
2024-06-05 16:48:15 +0900

    [Silver V] Title: 다리 놓기, Time: 84 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,21 +1,9 @@
-import sys
-input = sys.stdin.readline
+from math import comb
 
-# 다리는 겹치면 안된다.
-def bridge(n,m):
-    dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+def bridge(n, m):
+    return comb(m, n) 
 
-    for i in range(1,m+1):
-        dp[1][i] = i
-
-    for j in range(2,n+1):
-        for k in range(j,m+1):
-            for l in range(k,j-1,-1):
-                dp[j][k] += dp[j-1][l-1]
-
-    return dp[n][m]
-
-T = int(input().rstrip())
-for _ in range(T):          
-    n, m = map(int,input().rstrip().rsplit())
-    print(bridge(n,m)) 
+T = int(input())
+for _ in range(T):
+    n, m = map(int, input().split())
+    print(bridge(n, m))
