58bb790b78858e8fe47d2f3da5ffdfe865dcfd4b
kyongseo <102573192+kyongseo@users.noreply.github.com>
2024-06-07 10:24:13 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.39 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,9 +1,16 @@
 def solution(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+
     cnt = 0
-    check = {}
-    check[1] =1
-    check[2] =2
-    for i in range(3, n+1):
-        check[i] = check[i-2] + check[i-1]
+    check = [0] * (n + 1) 
+    check[1] = 1
+    check[2] = 2
+
+    for i in range(3, n + 1):
+        check[i] = check[i - 2] + check[i - 1]
+
     cnt = check[n] % 1234567
     return cnt
