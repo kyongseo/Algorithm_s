13c35a4725bf4627664f42912b0837b2c3cd7318
kyongseo <102573192+kyongseo@users.noreply.github.com>
2024-06-26 21:19:25 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4115.16 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,12 +1,11 @@
 def solution(elements):
-    extended_elements = elements + elements
-    unique_sums = set()  
-
-    n = len(elements)  
-
-    for length in range(1, n + 1):
-        for start in range(n):
-            part_sum = sum(extended_elements[start:start + length])
-            unique_sums.add(part_sum)
+    el = elements + elements
+    n = len(elements) # 5
+    answ = set()
+    
+    for i in range(1, n + 1):
+        for j in range(n):
+            to = sum(el[j:j+i])
+            answ.add(to)
             
-    return len(unique_sums)
+    return len(answ)
