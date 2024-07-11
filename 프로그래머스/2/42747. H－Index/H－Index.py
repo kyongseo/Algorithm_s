40afbc7983929eb1b34325dd230d7cd6277f25fc
kyongseo <pokj930@naver.com>
2024-07-11 23:44:24 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,8 +1,8 @@
 def solution(citations):
-    citations.sort()
-    article_count = len(citations) 
-
-    for i in range(article_count):
-        if citations[i] >= article_count-i:
-            return article_count-i
-    return 0
+    
+    citations.sort(reverse=True)
+    
+    for idx, citation in enumerate(citations):
+        if citation <= idx:
+            return idx
+    return len(citations)
