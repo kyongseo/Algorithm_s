eca3be8066f235682c9370787c53ec25bf2435a5
kyongseo <pokj930@naver.com>
2024-07-14 23:03:01 +0900

    [level 2] Title: 기능개발, Time: 0.76 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -2,13 +2,15 @@ def solution(progresses, speeds):
     answer = []
 
     while progresses:
-        cnt = 0
         for i in range(len(progresses)):
             progresses[i] += speeds[i]
-        while progresses and progresses[0] >= 100:
-            progresses.pop(0)
-            speeds.pop(0)
-            cnt += 1
-        if cnt > 0:
-                answer.append(cnt)
+
+        if progresses[0] >= 100:
+            count = 0
+            while progresses and progresses[0] >= 100:
+                progresses.pop(0)
+                speeds.pop(0)
+                count += 1
+            answer.append(count)
+        
     return answer
