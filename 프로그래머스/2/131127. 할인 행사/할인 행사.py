532a04fc92d2ac01e940578d8bd7945e276fa2c7
kyongseo <pokj930@naver.com>
2024-07-23 22:54:11 +0900

    [level 2] Title: 할인 행사, Time: 83.38 ms, Memory: 17.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,14 @@
+def solution(want, number, discount):
+    cnt = 0
+    s = []
+    for i in range(len(want)):
+        for j in range(number[i]):
+                s.append(want[i])
+        s.sort()
+    for i in range(len(discount) - 9):
+        list_10 = discount[i: i+10]
+        list_10.sort()
+        if s == list_10:
+            cnt += 1
+
+    return cnt
