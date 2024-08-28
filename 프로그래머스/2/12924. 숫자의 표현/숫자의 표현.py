d5b89acb8ce747ba9e0aca45b9ff596e540bcf31
kyongseo <pokj930@naver.com>
2024-08-28 21:19:54 +0900

    [level 2] Title: 숫자의 표현, Time: 6.36 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,13 @@
+def solution(n):     
+    count = 0
+
+    for i in range(1, n+1):
+        answer = 0
+        for j in range(i, n+1):
+            answer += j
+            if answer == n:
+                count += 1
+                break
+            if answer > n:
+                break
+    return count
