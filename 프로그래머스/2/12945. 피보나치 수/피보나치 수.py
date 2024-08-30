47bd93fe40017447610c9246dd4767d39ffe8be4
kyongseo <pokj930@naver.com>
2024-08-31 00:05:07 +0900

    [level 2] Title: 피보나치 수, Time: 20.56 ms, Memory: 13.6 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -3,7 +3,5 @@ def solution(n):
     dp[0] = 0
     dp[1] = 1
     for i in range(2, n+1):
-        dp[i] = (dp[i-1] + dp[i-2]) % 1234567
-
-    
+        dp[i] = (dp[i-2] + dp[i-1]) % 1234567
     return dp[n]
