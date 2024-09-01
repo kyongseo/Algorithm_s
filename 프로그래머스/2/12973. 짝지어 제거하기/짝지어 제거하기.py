9cc5624c8a2d31efc8e86a9c11dd913c38e91332
kyongseo <pokj930@naver.com>
2024-09-01 22:58:22 +0900

    [level 2] Title: 짝지어 제거하기, Time: 156.35 ms, Memory: 24 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,15 @@
+def solution(s):
+    stack = []
+    s = list(s)
+    for i in s:
+        if len(stack) == 0:
+            stack.append(i)
+        elif i == stack[-1]:
+            stack.pop()
+        else:
+            stack.append(i)
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0 
+                  
