93b96fdd09cd546fefb73f948fda6c73c036092f
kyongseo <pokj930@naver.com>
2024-09-11 16:49:44 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,17 +1,10 @@
 def solution(n, words):
-    answer = [0,0]
 
-    cnt = 0  
-    checks = []  
-    checks.append(words[0])
-    for i in range(1, len(words)):  # 단어 순회하면서
-        cnt += 1
-        
-        if words[i] not in checks and list(words[i-1])[-1] == list(words[i])[0]:
-            checks.append(words[i])
-        else: 
-            answer[0] = cnt%n +1  
-            answer[1] = cnt//n +1  
-            break
+    a = [words[0]]
+    for i in range(1, len(words)):
+        if words[i][0] == words[i-1][-1] and words[i] not in a:
+            a.append(words[i])
+        else:
+            return [(i%n)+1, (i//n)+1]
 
-    return answer
+    return [0,0]
