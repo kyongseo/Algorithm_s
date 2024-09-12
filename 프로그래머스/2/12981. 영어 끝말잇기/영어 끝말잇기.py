7b23c40e7635cf22cb06306c5ce5468a1ea8fbc3
kyongseo <pokj930@naver.com>
2024-09-12 17:41:58 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.18 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,9 +1,11 @@
 def solution(n, words):
-
-    a = [words[0]]
+    
+    tem = []
+    tem.append(words[0])
+    
     for i in range(1, len(words)):
-        if words[i][0] == words[i-1][-1] and words[i] not in a:
-            a.append(words[i])
+        if words[i-1][-1] == words[i][0] and words[i] not in tem:
+            tem.append(words[i])
         else:
             return [(i%n)+1, (i//n)+1]
 
