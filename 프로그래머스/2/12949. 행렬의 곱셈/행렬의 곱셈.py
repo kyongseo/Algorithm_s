7cf412a9e48fefbcc992468cb84110b66035a70a
kyongseo <pokj930@naver.com>
2024-09-13 10:04:28 +0900

    [level 2] Title: 행렬의 곱셈, Time: 131.33 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,15 +1,14 @@
 def solution(arr1, arr2):
-
-    m = len(arr1)
-    n = len(arr1[0])
-
-    p = len(arr2[0])
-
-    result = [[0] * p for _ in range(m)]
-
-    for i in range(m):
-        for j in range(p):
-            for k in range(n):
-                result[i][j] += arr1[i][k] * arr2[k][j]
-
-    return result
+    
+    m = len(arr1) # 행의 수 
+    n = len(arr1[0]) # 열의 수
+    p = len(arr2[0]) # 열의 수 
+    
+    
+    answer = [[0] * p for _ in range(m)]
+    
+    for m2 in range(m):
+        for n2 in range(n):
+            for p2 in range(p):
+                answer[m2][p2] +=  arr1[m2][n2] * arr2[n2][p2]
+    return answer
