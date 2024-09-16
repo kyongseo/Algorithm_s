4b35f92940bfb1c618e896e2edb8b89309779f46
kyongseo <pokj930@naver.com>
2024-09-17 00:14:43 +0900

    [level 2] Title: 귤 고르기, Time: 40.99 ms, Memory: 26.5 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,13 +1,11 @@
-from collections import Counter as c
+from collections import Counter
 
 def solution(k, tangerine):
-    answer = 0 
-    for arr in c(tangerine).most_common():
-        
-        k -= arr[1]
+    answer = 0
+    for irr in Counter(tangerine).most_common():
+        k -= irr[1]
         answer += 1
 
         if k <= 0:
             break
-            
     return answer
