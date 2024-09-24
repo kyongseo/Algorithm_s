5f196eaa575b231aef838f9caad5c6e2e96f38b1
kyongseo <pokj930@naver.com>
2024-09-24 11:32:37 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,12 +1,13 @@
 def solution(clothes):
     answer = 1
-    dic = {}
-    for i in clothes:
-        if i[1] in dic:
-            dic[i[1]] += 1
+    dict = {}
+    for cloth in clothes:
+        if cloth[1] in dict:
+            dict[cloth[1]] += 1
         else:
-            dic[i[1]] = 1
-    for i in dic.keys():
-        answer *= dic[i] + 1
-        
-    return answer - 1
+            dict[cloth[1]] = 1
+
+    for key in dict.keys():
+        answer *= (dict[key] + 1)
+
+    return answer -1
