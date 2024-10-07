3a722409e04e7d7b5361d9b0e2b91413fe74f7b1
kyongseo <pokj930@naver.com>
2024-10-07 22:36:00 +0900

    [level 2] Title: 피로도, Time: 39.30 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,18 +1,16 @@
 from itertools import permutations
 
 def solution(k, dungeons):
+    
     answer = 0 
-
     for dun in permutations(dungeons):
-
+        count = k
         ans = 0
-        tmp = k
-        for min, use in dun:
-            if tmp >= min:
-                tmp -= use
+        for order, use in dun:
+            if count >= order:
+                count -= use
                 ans += 1
+                
         if ans > answer:
             answer = ans
-
     return answer
-
